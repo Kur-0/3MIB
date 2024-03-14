608e0f9a86112a1ed51768c97960156c1163116b
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-14 08:27:31 -0300

    mexendo mais com algumas coisinhas da ionic (notificações e alerts)

---
+++ toastalert/src/app/home/home.page.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {ToastController, AlertController} from '@ionic/angular';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  radioSelecionada:string='';
+  constructor(
+    public toastController: ToastController,
+    public alertController: AlertController
+  ) {}
+
+  verificarRadio(){
+    var mensagem='';
+    if(this.radioSelecionada=='option1'){
+      mensagem='Você clicou na radio 1';
+    }
+    else if(this.radioSelecionada=='option2'){
+      mensagem='Você clicou na radio 2';
+    }
+    else{
+      mensagem='Você clicou na radio 3';
+    }
+    this.exibirToast(mensagem);
+    this.exibirAlerta(mensagem);
+  }
+
+  async exibirToast(mensagem:string){
+    const toast = await this.toastController.create({
+      message: mensagem,
+      duration: 2000,
+      position: "top",
+      color: "danger",
+    });
+    toast.present();
+  }
+
+  async exibirAlerta(mensagem:string){
+    const alert = await this.alertController.create({
+      header: 'Esta é uma mensagem de exemplo',
+      message: mensagem,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+}
