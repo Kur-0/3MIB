c4180e689fa71119a48d525f8de55dd9a0d85ebe
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-06 12:58:24 -0300

    Reactando dnv

---
+++ WEBDEV/CineApp/src/componentes/Header/Header.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Navbar/Navbar";
+
+function Header() {
+    return ( 
+        <header>
+            <span>Olá, visitante!</span>
+            <h1>Bem vindo ao Chlonema</h1>
+            <Navbar></Navbar>
+        </header>
+     );
+}
+
+export default Header;
