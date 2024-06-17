2bb801e969b30745fdb97249b2fd1b1b55d72142
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-17 12:53:36 -0300

    finalizando a pagina individual de cada filme!

--- WEBDEV/CineApp/src/Pages/paginaFilme.jsx
+++ WEBDEV/CineApp/src/Pages/paginaFilme.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function PaginaFilmes(){
+
+    const {id} = useParams();
+    const [filme, setFilme] = useState([])
+
+    const apiKey='api_key=7c572a9f5b3ba776080330d23bb76e1e'
+    const urlBase = 'https://api.themoviedb.org/3/movie/'
+    const urlImg = 'https://image.tmdb.org/t/p/w1280/'
+
+
+    useEffect( () => {
+        fetch(`${urlBase}${id}?${apiKey}&language=pt-BR`)
+        .then(response => response.json())
+        .then(response => setFilme(response))
+        .catch(erro => console.log(erro))
+    },[])
+
     return(
-        <h1> Página do Filme</h1>
-    )
+        <>
+        <img className="w-auto" src={`${urlImg}${filme.backdrop_path}`}/>
+        <h1> {filme.title} </h1>
+        <p>{filme.overview}</p>
+        </>
+    );
 }
 
 export default PaginaFilmes
