f26eccf13722ab83be350d7dc89feeb155184a73
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 12:15:56 -0300

    marlon

--- GAMES/FlappyBird/Assets/scripts/menu.cs
+++ GAMES/FlappyBird/Assets/scripts/menu.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements; // Corrigido o namespace
 
 public class menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Button btnPlay;
+    private Button btnScore;
+
+    void OnEnable()
     {
-        
+        // Pegando a raiz do documento UI
+        var root = GetComponent<UIDocument>().rootVisualElement;
+
+        // Pegando o botão pela referência do nome
+        btnPlay = root.Q<Button>("btnPlay");
+        btnScore = root.Q<Button>("btnScore");
+
+        if (btnScore != null)
+        {
+            btnScore.clicked += LoadSceneB;
+        }
+        else
+        {
+            Debug.LogError("Botão btnScore não encontrado!");
+        }
+
+        if (btnPlay != null)
+        {
+            btnPlay.clicked += LoadSceneA;
+        }
+        else
+        {
+            Debug.LogError("Botão btnPlay não encontrado!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadSceneA()
     {
-        
+        SceneManager.LoadScene("a");
+    }
+
+    void LoadSceneB()
+    {
+        SceneManager.LoadScene("ScoreScene");
     }
 }
